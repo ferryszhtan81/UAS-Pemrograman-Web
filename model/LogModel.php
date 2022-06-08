dfc9575d5e7f34ad01dbebc26f0acdd0e342920b
Muhammad Hafizh <90849858+HfzYan@users.noreply.github.com>
2022-06-08 11:29:09 +0800

    Update LogModel.php

--- model/LogModel.php
+++ model/LogModel.php
@@ -1,16 +1,44 @@
 <?php
-function tambahLog()
+session_start();
+
+function ambilDataLog($koneksi)
+{
+    $fetch = mysqli_query($koneksi, "SELECT * FROM log");
+    $i = 0;
+    while ($row = mysqli_fetch_assoc($fetch)) {
+        $rows[$i] = $row;
+        $i++;
+    }
+    if ($i != 0) {
+        return $rows;
+    } else {
+        return false;
+    }
+}
+
+function ambilNamaUser($koneksi, $id)
+{
+    $fetch = mysqli_query($koneksi, "SELECT nama_user FROM user WHERE id = $id");
+    $result = mysqli_fetch_assoc($fetch);
+    return $result['nama_user'];
+}
+
+function loginLog($koneksi)
 {
 }
 
-function updateLog()
+function tambahLog($koneksi)
 {
 }
 
-function hapusLog()
+function updateLog($koneksi)
 {
 }
 
-function tambahKategoriLog()
+function hapusLog($koneksi)
+{
+}
+
+function tambahKategoriLog($koneksi)
 {
 }
