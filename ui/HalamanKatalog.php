fe8e24b8400ff9fa841f93f08829a5ae6fa6dab3
Ferry Roland <101640525+ferryszhtan81@users.noreply.github.com>
2022-06-10 07:58:18 +0800

    pembetulan error

--- ui/HalamanKatalog.php
+++ ui/HalamanKatalog.php
@@ -5,6 +5,7 @@ require '../model/KatalogModel.php';
 require '../helper/Model.php';
 
 isLogin("HalamanHome.php");
+$koneksi = mysqli_connect('localhost', 'root', '', 'minimarket');
 $data = ambilBarang($koneksi);
 $dataInfo = ambilInfoBarang($koneksi);
 ?>
