9c8ce4ccd916125fe1687e119a67b7c2d106d48b
Ferry Roland <101640525+ferryszhtan81@users.noreply.github.com>
2022-06-10 09:16:20 +0800

    Create HapusItemKatalog.php

---
+++ ui/HapusItemKatalog.php
@@ -0,0 +1,26 @@
+<?php
+require '../helper/Koneksi.php';
+require '../model/KatalogModel.php';
+require '../helper/Model.php';
+
+isLogin("HalamanHome.php");
+$koneksi = mysqli_connect('localhost', 'root', '', 'minimarket');
+$data = ambilBarang($koneksi);
+$dataInfo = ambilInfoBarang($koneksi);
+
+if($_POST){
+
+    <?php
+require '../helper/Koneksi.php';
+require '../model/KatalogModel.php';
+require '../helper/Model.php';
+
+isLogin("HalamanHome.php");
+$koneksi = mysqli_connect('localhost', 'root', '', 'minimarket');
+$data = ambilBarang($koneksi);
+$dataInfo = ambilInfoBarang($koneksi);
+
+if($_POST){
+    hapusBarang($id, $koneksi);
+}
+?>
